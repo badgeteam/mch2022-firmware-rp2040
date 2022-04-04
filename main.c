7675719532bae9c4aeb4b11dbb8f35db90295317
Jana-Marie <janamarie@janamarie.dev>
2022-04-04 13:31:53 +0200

    cleaned main imports

--- main.c
+++ main.c
@@ -1,22 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include "main.h"
 
-#include "hardware/irq.h"
-#include "hardware.h"
-
-#include "bsp/board.h"
-#include "tusb.h"
-
-//------------- prototypes -------------//
-static void cdc_task(void);
-
-/*------------- MAIN -------------*/
 int main(void)
 {
   board_init();
-
   tusb_init();
 
   while (1)
@@ -26,21 +12,4 @@ int main(void)
   }
 
   return 0;
-}
-
-static void cdc_task(void)
-{
-  uint8_t itf;
-
-  for (itf = 0; itf < CFG_TUD_CDC; itf++)
-  {
-    {
-      if ( tud_cdc_n_available(itf) )
-      {
-        uint8_t buf[64];
-
-        uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));
-      }
-    }
-  }
 }
---
+++ main.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "hardware/irq.h"
+#include "hardware.h"
+
+#include "bsp/board.h"
+#include "tusb.h"
+#include "usb_cdc.h"
