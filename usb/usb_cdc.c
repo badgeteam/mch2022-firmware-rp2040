93ce38f47653321b4476efa21bbee9bed12dbad6
Jana-Marie <janamarie@janamarie.dev>
2022-04-25 13:59:25 +0200

    updated usb

--- usb/usb_cdc.c
+++ usb/usb_cdc.c
@@ -8,11 +8,20 @@ void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
 }
 
 // interrupt based usb cdc
-void tud_cdc_rx_cb(uint8_t itf){
+void tud_cdc_rx_cb(uint8_t itf){ // itf is always 0, why?
   if (itf == USB_CDC_ESP32){
     char buf[64];
-    uint32_t count = tud_cdc_read(buf, sizeof(buf));
-    tud_cdc_write(buf, count);
+    uint32_t len = tud_cdc_read(buf, sizeof(buf));
+    esp32_uart_tx(buf, len);
+  } else if (itf == USB_CDC_FPGA){ // this does not work, why?
+  } else if (itf == USB_CDC_STDIO){
+  } else {
+  }
+}
+
+void tud_cdc_tx_cb(uint8_t itf, char *buf, size_t len){
+  if (itf == USB_CDC_ESP32){
+    tud_cdc_write(buf, len);
     tud_cdc_write_flush();
   } else if (itf == USB_CDC_FPGA){
 
--- usb/usb_cdc.h
+++ usb/usb_cdc.h
@@ -1,3 +1,5 @@
 #pragma once
 
 #include "../main.h"
+
+void tud_cdc_tx_cb(uint8_t itf, char *buf, size_t len);
