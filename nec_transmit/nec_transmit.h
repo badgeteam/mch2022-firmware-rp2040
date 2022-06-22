773d9fbb9c00c9730e2985068b78e94610ddc227
Renze Nicolai <renze@rnplus.nl>
2022-06-23 01:15:58 +0200

    Apply formatting

--- nec_transmit/nec_transmit.h
+++ nec_transmit/nec_transmit.h
@@ -1,9 +1,9 @@
-#include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "pico/stdlib.h"
 
 // public API
 //
-int nec_tx_init(PIO, uint);
+int  nec_tx_init(PIO, uint);
 void nec_tx(PIO, int, uint8_t, uint8_t);
 void nec_tx_extended(PIO, int, uint16_t, uint16_t);
 void nec_tx_raw(PIO, int, uint32_t);
